9f176c53a7bdd31f6f7b16c090039789b23d9b1e
ziagham <amin.ziagham@gmail.com>
2023-11-14 23:53:39 +0100

    Add ConfigurationException to handle config-related Exceptions.

---
+++ src/FlowSync.Core/Exceptions/ConfigurationException.cs
@@ -0,0 +1,8 @@
+﻿namespace FlowSync.Core.Exceptions;
+
+public class ConfigurationException : Exception
+{
+    public ConfigurationException() { }
+    public ConfigurationException(string message) : base(message) { }
+    public ConfigurationException(string message, Exception inner) : base(message, inner) { }
+}
