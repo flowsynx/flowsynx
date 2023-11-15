c556c23378d235180ad9b6f3be029049e1b26675
ziagham <amin.ziagham@gmail.com>
2023-11-15 23:27:07 +0100

    Delete configuration property implementation from ILocation service

--- src/FlowSync/Services/FlowSyncLocation.cs
+++ src/FlowSync/Services/FlowSyncLocation.cs
@@ -1,75 +1,31 @@
-﻿using System.Reflection;
-using FlowSync.Core.Serialization;
-using FlowSync.Core.Services;
+﻿using FlowSync.Core.Services;
 
 namespace FlowSync.Services;
 
 public class FlowSyncLocation : ILocation
 {
     private readonly ILogger<FlowSyncLocation> _logger;
-    private readonly ISerializer _serializer;
-    private readonly string? _rootLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-    private readonly string _configurationFile;
+    private readonly string? _rootLocation = Path.GetDirectoryName(System.AppContext.BaseDirectory);
 
-    public FlowSyncLocation(ILogger<FlowSyncLocation> logger, ISerializer serializer)
+    public FlowSyncLocation(ILogger<FlowSyncLocation> logger)
     {
         _logger = logger;
-        _serializer = serializer;
 
         if (_rootLocation == null)
         {
             logger.LogError("Base location not found");
             throw new Exception("Base location not found");
         }
-
-        _configurationFile = Path.Combine(_rootLocation, "configuration.json");
-        CheckConfigurationFile(_configurationFile);
     }
 
     public string RootLocation => GetRootLocation();
 
-    public string ConfigurationFile => GetConfigurationFile();
-
     #region MyRegion
     private string GetRootLocation()
     {
-        if (_rootLocation is null)
-            throw new Exception("Root location not found");
-        return _rootLocation;
-    }
-    
-    private string GetConfigurationFile()
-    {
-        CheckConfigurationFile(_configurationFile);
-        return _configurationFile;
-    }
-
-    private void CheckLocation(string location)
-    {
-        try
-        {
-            if (!Directory.Exists(location))
-                Directory.CreateDirectory(location);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError($"Error in Checking location: {e.Message}");
-            throw;
-        }
-    }
-
-    private void CheckConfigurationFile(string path)
-    {
-        try
-        {
-            if (!File.Exists(path))
-                File.WriteAllText(path, _serializer.Serialize(new { }));
-        }
-        catch (Exception e)
-        {
-            _logger.LogError($"Error in Checking configuration file: {e.Message}");
-            throw;
-        }
+        if (_rootLocation is not null) return _rootLocation;
+        _logger.LogError("Root location not found");
+        throw new Exception("Root location not found");
     }
     #endregion
 }
