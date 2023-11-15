74b46dc30470a796ff54a61fc3b5b9748f7cd4e2
ziagham <amin.ziagham@gmail.com>
2023-11-15 23:21:35 +0100

    Rename application extensions class, movehealthcheck function to application extensions, as well update service collection extesnions such as remove swagger functionality.

---
+++ src/FlowSync/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,63 @@
+﻿using FlowSync.Core.Serialization;
+using FlowSync.HealthCheck;
+using FlowSync.Middleware;
+using FlowSync.Models;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace FlowSync.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static IApplicationBuilder UseCustomException(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionMiddleware>();
+        return app;
+    }
+
+    public static IApplicationBuilder UseCustomHeaders(this IApplicationBuilder builder)
+    {
+        var headers = new CustomHeadersToAddAndRemove();
+        headers.HeadersToAdd.Add("FlowSync-Version", "v1.1");
+
+        builder.UseMiddleware<CustomHeadersMiddleware>(headers);
+        return builder;
+    }
+
+    public static IApplicationBuilder UseHealthCheck(this IApplicationBuilder app)
+    {
+        app.UseEndpoints(endpoints => {
+            var serviceProvider = app.ApplicationServices;
+            var serializer = serviceProvider.GetService<ISerializer>();
+            if (serializer == null)
+                throw new ArgumentException("Serializer could not be initialized.");
+                
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions
+            {
+                ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                    [HealthStatus.Degraded] = StatusCodes.Status200OK,
+                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                },
+                ResponseWriter = async (context, report) =>
+                {
+                    context.Response.ContentType = serializer.ContentMineType;
+                    var response = new HealthCheckResponse
+                    {
+                        Status = report.Status.ToString(),
+                        HealthChecks = report.Entries.Select(x => new IndividualHealthCheckResponse
+                        {
+                            Component = x.Key,
+                            Status = x.Value.Status.ToString(),
+                            Description = x.Value.Description
+                        }),
+                        HealthCheckDuration = report.TotalDuration
+                    };
+                    await context.Response.WriteAsync(serializer.Serialize(response));
+                }
+            });
+        });
+        return app;
+    }
+}
--- src/FlowSync/Extensions/ServiceCollectionExtensions.cs
+++ src/FlowSync/Extensions/ServiceCollectionExtensions.cs
@@ -1,53 +1,50 @@
-﻿using Asp.Versioning;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Reflection;
-using FlowSync.Core.Services;
+﻿using FlowSync.Core.Services;
 using FlowSync.Services;
-using FlowSync.Swagger;
+using FlowSync.Enums;
+using Serilog;
+using Serilog.Events;
+using FlowSync.HealthCheck;
 
 namespace FlowSync.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddAndConfigApiVersioning(this IServiceCollection services, ApiVersion version)
-    {
-        services.Configure<RouteOptions>(options => { options.LowercaseUrls = true; });
-
-        services.AddApiVersioning(
-                options =>
-                {
-                    options.DefaultApiVersion = version;
-                    options.ApiVersionReader = new UrlSegmentApiVersionReader();
-                    options.ReportApiVersions = true;
-                    options.AssumeDefaultVersionWhenUnspecified = true;
-                })
-            .AddApiExplorer(
-                options =>
-                {
-                    options.GroupNameFormat = "'v'VVV";
-                    options.SubstituteApiVersionInUrl = true;
-                })
-            .EnableApiVersionBinding();
-
-        return services;
-    }
-
-    public static IServiceCollection AddAndConfigSwagger(this IServiceCollection services)
-    {
-        services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
-        services.AddSwaggerGen(options =>
-        {
-            options.OperationFilter<SwaggerDefaultValues>();
-        });
-
-        return services;
-    }
-
     public static IServiceCollection AddLocation(this IServiceCollection services)
     {
         services.AddTransient<ILocation, FlowSyncLocation>();
+        return services;
+    }
+
+    public static IServiceCollection AddLoggingService(this IServiceCollection services, bool enable, AppLogLevel logLevel)
+    {
+        var level = logLevel switch
+        {
+            AppLogLevel.All => LogEventLevel.Verbose,
+            AppLogLevel.Debug => LogEventLevel.Debug,
+            AppLogLevel.Error => LogEventLevel.Error,
+            AppLogLevel.Fatal => LogEventLevel.Fatal,
+            AppLogLevel.Information => LogEventLevel.Information,
+            AppLogLevel.Warning => LogEventLevel.Warning,
+            _ => LogEventLevel.Verbose
+        };
+
+        services.AddLogging(c => c.ClearProviders());
+
+        var logger = new LoggerConfiguration()
+            .WriteTo.Conditional(_ => enable,
+                config => config.Console(restrictedToMinimumLevel: level, outputTemplate: "[time={Timestamp:HH:mm:ss} level={Level}] message=\"{Message}\"{NewLine}{Exception}"))
+            .CreateLogger();
+
+        services.AddSerilog(logger);
+        return services;
+    }
+
+    public static IServiceCollection AddHealthChecker(this IServiceCollection services)
+    {
+        services
+            .AddHealthChecks()
+            .AddCheck<ConfigurationManagerHealthCheck>(name: "Configuration Registry")
+            .AddCheck<PluginsManagerHealthCheck>(name: "Plugins Registry");
         return services;
     }
 }
