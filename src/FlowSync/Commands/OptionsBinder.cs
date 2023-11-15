4566490bad4bc26f0a68f8505b087aa7d7c3ad65
ziagham <amin.ziagham@gmail.com>
2023-11-15 23:05:45 +0100

    Add root command line with binder

---
+++ src/FlowSync/Commands/OptionsBinder.cs
@@ -0,0 +1,34 @@
+﻿using System.CommandLine.Binding;
+using System.CommandLine;
+using FlowSync.Enums;
+using FlowSync.Core.Serialization;
+
+namespace FlowSync.Commands;
+
+public class OptionsBinder : BinderBase<CommandOptions>
+{
+    private readonly Option<int> _port;
+    private readonly Option<string> _config;
+    private readonly Option<bool> _enableLog;
+    private readonly Option<AppLogLevel> _logLevel;
+
+    public OptionsBinder(Option<int> port, Option<string> config, Option<bool> enableLog, Option<AppLogLevel> logLevel)
+    {
+        _port = port;
+        _config = config;
+        _enableLog = enableLog;
+        _logLevel = logLevel;
+    }
+
+    protected override CommandOptions GetBoundValue(BindingContext bindingContext)
+    {
+        var serializer = bindingContext.GetService<ISerializer>();
+        return new CommandOptions
+        {
+            Port = bindingContext.ParseResult.GetValueForOption(_port),
+            Config = bindingContext.ParseResult.GetValueForOption(_config) ?? string.Empty,
+            EnableLog = bindingContext.ParseResult.GetValueForOption(_enableLog),
+            AppLogLevel = bindingContext.ParseResult.GetValueForOption(_logLevel)
+        };
+    }
+}
---
+++ src/FlowSync/Commands/Root.cs
@@ -0,0 +1,38 @@
+﻿using System.CommandLine;
+using FlowSync.ApplicationBuilders;
+using FlowSync.Enums;
+using FlowSync.Models;
+using FlowSync.Services;
+
+namespace FlowSync.Commands;
+
+public class Root : RootCommand
+{
+    public Root(ILogger<Root> logger, IOptionsVerifier optionsVerifier, IApiApplicationBuilder apiApplicationBuilder) 
+        : base("Command Line Interface application for Cloud Storage")
+    {
+        var portOption = new Option<int>(name: "--port", getDefaultValue: () => 4400, description: "The port FlowSync is listening on");
+        var configOption = new Option<string>(name: "--config-file", description: "FlowSync configuration file");
+        var enableLogOption = new Option<bool>(name: "--enable-log", getDefaultValue: () => true, description: "Enable logging to records the details of events during application running");
+        var logLevelOption = new Option<AppLogLevel>(name: "--log-level", getDefaultValue: () => AppLogLevel.Information, description: "The log verbosity to controls the amount of detail emitted for each event that is logged");
+
+        this.AddOption(portOption);
+        this.AddOption(enableLogOption);
+        this.AddOption(logLevelOption);
+        this.AddOption(configOption);
+
+        this.SetHandler(async (options) =>
+        {
+            try
+            {
+                optionsVerifier.Verify(ref options);
+                await apiApplicationBuilder.RunAsync(options);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+            }
+        },
+        new OptionsBinder(portOption, configOption, enableLogOption, logLevelOption));
+    }
+}
