9eea66cebfb516635db3f609481dabd34b2063ed
ziagham <amin.ziagham@gmail.com>
2023-11-15 23:30:13 +0100

    Adding and removing customer header model.

---
+++ src/FlowSync/Models/CustomHeadersToAddAndRemove.cs
@@ -0,0 +1,7 @@
+﻿namespace FlowSync.Models;
+
+public class CustomHeadersToAddAndRemove
+{
+    public Dictionary<string, string> HeadersToAdd = new();
+    public HashSet<string> HeadersToRemove = new();
+}
