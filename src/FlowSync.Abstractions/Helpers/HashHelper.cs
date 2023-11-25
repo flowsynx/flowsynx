df05261851beb9e5551c787a5ffa3e80d551d6cb
ziagham <amin.ziagham@gmail.com>
2023-11-25 21:42:33 +0100

    Add hashing helper

---
+++ src/FlowSync.Abstractions/Helpers/HashHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace FlowSync.Abstractions.Helpers;
+
+public static class HashHelper
+{
+    public static string CreateMd5(string input)
+    {
+        using var md5 = System.Security.Cryptography.MD5.Create();
+        var inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        var hashBytes = md5.ComputeHash(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
+    }
+}
