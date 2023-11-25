b0c652d0b44e7cb819ab32ebb438709b27f2f0ee
ziagham <amin.ziagham@gmail.com>
2023-11-25 21:45:11 +0100

    Rename file system filter option

---
+++ src/FlowSync.Abstractions/FileSystemFilterOptions.cs
@@ -0,0 +1,32 @@
+﻿using FlowSync.Abstractions.Entities;
+using FlowSync.Abstractions.Helpers;
+
+namespace FlowSync.Abstractions;
+
+public class FileSystemFilterOptions
+{
+    private string _directoryPath = PathHelper.RootDirectoryPath;
+
+    public string DirectoryPath
+    {
+        get => _directoryPath;
+        set => _directoryPath = PathHelper.Normalize(value);
+    }
+
+    public FilterItemKind Kind { get; set; } = FilterItemKind.FileAndDirectory;
+    public string? Include { get; set; }
+    public string? Exclude { get; set; }
+    public string? MinimumAge { get; set; }
+    public string? MaximumAge { get; set; }
+    public string? MinimumSize { get; set; }
+    public string? MaximumSize { get; set; }
+    public string? Sorting { get; set; }
+    public bool CaseSensitive { get; set; } = false;
+    public bool Recurse { get; set; } = false;
+    public int MaxResults { get; set; } = 10;
+
+    public FileSystemFilterOptions Clone()
+    {
+        return (FileSystemFilterOptions)MemberwiseClone();
+    }
+}
