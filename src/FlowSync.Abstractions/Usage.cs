df66158a9f7192a57ad797721bc185bde6d5dcb0
ziagham <amin.ziagham@gmail.com>
2023-11-25 21:44:41 +0100

    Add usage model in order to use in filesystem about functionality

--- src/FlowSync.Abstractions/Usage.cs
+++ src/FlowSync.Abstractions/Usage.cs
@@ -2,10 +2,7 @@
 
 public class Usage
 {
-    public long Total { get; set; }     // quota of bytes that can be used
-    public long Used { get; set; }      // bytes in use
-    public long Trashed { get; set; }   // bytes in trash
-    public long Other { get; set; }     // other usage e.g. gmail in drive
-    public long Free { get; set; }      // bytes which can be uploaded before reaching the quota
-    public long Objects { get; set; }   // objects in the storage system
+    public long Total { get; set; }
+    public long Free { get; set; }
+    public long Used => Total - Free;
 }
