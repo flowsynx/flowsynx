e9c69a8e0e0c429d007c5a8d6662376aac21d4e3
ziagham <amin.ziagham@gmail.com>
2023-11-25 21:41:08 +0100

    Remove unnecessary project

--- src/FlowSyncConsole/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using FlowSyncConsole;
-
-using IFileSystem fs = new PhysicalFileSystem("D:");
-var list = fs.GetEntities(new FileSystemPath());
-
-foreach (var item in list)
-{
-    Console.WriteLine(item.Path);
-}
