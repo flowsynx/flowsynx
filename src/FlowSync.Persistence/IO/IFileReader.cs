86a61286cb55e938c70aed65929e182bedfd7ba1
ziagham <amin.ziagham@gmail.com>
2023-11-27 18:05:02 +0100

    Remove project due to move classes and codes to infrastructure project

--- src/FlowSync.Persistence/IO/IFileReader.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FlowSync.Persistence.Json.IO;
-
-public interface IFileReader
-{
-    public string Read(string path);
-}
--- src/FlowSync.Persistence/IO/IFileWriter.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FlowSync.Persistence.Json.IO;
-
-public interface IFileWriter
-{
-    public bool Write(string path, string contents);
-}
