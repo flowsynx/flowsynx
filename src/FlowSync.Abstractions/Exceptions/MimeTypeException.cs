3aa28175056a2a52a5b588b7b5efc19a8825a16d
ziagham <amin.ziagham@gmail.com>
2023-12-10 12:35:51 +0100

    Add exception class for mimetype

---
+++ src/FlowSync.Abstractions/Exceptions/MimeTypeException.cs
@@ -0,0 +1,7 @@
+﻿namespace FlowSync.Abstractions.Exceptions;
+
+public class MimeTypeException : FlowSyncBaseException
+{
+    public MimeTypeException(string message) : base(message) { }
+    public MimeTypeException(string message, Exception inner) : base(message, inner) { }
+}
