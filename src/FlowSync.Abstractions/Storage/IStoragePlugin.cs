82efef7a8b4d83956c150bb6d0dd2574f9cb19a6
ziagham <amin.ziagham@gmail.com>
2023-12-10 12:34:31 +0100

    Refactoring and adding some other abstract methods

--- src/FlowSync.Abstractions/Storage/IStoragePlugin.cs
+++ src/FlowSync.Abstractions/Storage/IStoragePlugin.cs
@@ -4,11 +4,13 @@ public interface IStoragePlugin : IPlugin, IDisposable
 {
     Specifications? Specifications { get; set; }
     Task<StorageUsage> About(CancellationToken cancellationToken = default);
-    Task<IEnumerable<StorageEntity>> ListAsync(string path, StorageSearchOptions searchOptions, int? maxResult, CancellationToken cancellationToken = default);
-    Task WriteAsync(string path, StorageStream storageStream, bool append = false, CancellationToken cancellationToken = default);
+    Task<IEnumerable<StorageEntity>> ListAsync(string path, StorageSearchOptions searchOptions, StorageListOptions listOptions, CancellationToken cancellationToken = default);
+    Task WriteAsync(string path, StorageStream storageStream, CancellationToken cancellationToken = default);
     Task<StorageStream> ReadAsync(string path, CancellationToken cancellationToken = default);
+    Task<bool> FileExistAsync(string path, CancellationToken cancellationToken = default);
     Task DeleteAsync(string path, StorageSearchOptions storageSearches, CancellationToken cancellationToken = default);
     Task DeleteFileAsync(string path, CancellationToken cancellationToken = default);
-    Task CreateDirectoryAsync(string path, CancellationToken cancellationToken = default);
-    Task DeleteDirectoryAsync(string path, CancellationToken cancellationToken = default);
+    Task MakeDirectoryAsync(string path, CancellationToken cancellationToken = default);
+    Task PurgeDirectoryAsync(string path, CancellationToken cancellationToken = default);
+    Task<bool> DirectoryExistAsync(string path, CancellationToken cancellationToken = default);
 }
