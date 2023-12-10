a67190a28a448a3553b8256f4c2d07060ea8e1ab
ziagham <amin.ziagham@gmail.com>
2023-12-10 12:38:56 +0100

    Modify the search options and move the some properties to the list options

--- src/FlowSync.Abstractions/Storage/StorageSearchOptions.cs
+++ src/FlowSync.Abstractions/Storage/StorageSearchOptions.cs
@@ -4,19 +4,12 @@ namespace FlowSync.Abstractions.Storage;
 
 public class StorageSearchOptions
 {
-    public StorageFilterItemKind Kind { get; set; } = StorageFilterItemKind.FileAndDirectory;
     public string? Include { get; set; }
     public string? Exclude { get; set; }
     public string? MinimumAge { get; set; }
     public string? MaximumAge { get; set; }
     public string? MinimumSize { get; set; }
     public string? MaximumSize { get; set; }
-    public string? Sorting { get; set; }
     public bool CaseSensitive { get; set; } = false;
     public bool Recurse { get; set; } = false;
-
-    public StorageSearchOptions Clone()
-    {
-        return (StorageSearchOptions)MemberwiseClone();
-    }
 }
