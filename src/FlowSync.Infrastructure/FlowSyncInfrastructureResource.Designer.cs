c9be058da8b05c7c389d56275bf975a0cdbb9876
ziagham <amin.ziagham@gmail.com>
2023-12-10 12:46:11 +0100

    Add and edit some part of messages in resource file.

--- src/FlowSync.Infrastructure/FlowSyncInfrastructureResource.Designer.cs
+++ src/FlowSync.Infrastructure/FlowSyncInfrastructureResource.Designer.cs
@@ -61,83 +61,56 @@ namespace FlowSync.Infrastructure {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to {0} is not found..
+        /// </summary>
+        internal static string ConfigurationManagerItemNotFoumd {
+            get {
+                return ResourceManager.GetString("ConfigurationManagerItemNotFoumd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The value from ({0}) could not be extracted!.
         /// </summary>
-        internal static string FileSystemDateParserCannotExtractValue {
+        internal static string DateParserCannotExtractValue {
             get {
-                return ResourceManager.GetString("FileSystemDateParserCannotExtractValue", resourceCulture);
+                return ResourceManager.GetString("DateParserCannotExtractValue", resourceCulture);
             }
         }
         
         /// <summary>
         ///   Looks up a localized string similar to The given datetime is not valid!.
         /// </summary>
-        internal static string FileSystemDateParserInvalidInput {
+        internal static string DateParserInvalidInput {
             get {
-                return ResourceManager.GetString("FileSystemDateParserInvalidInput", resourceCulture);
+                return ResourceManager.GetString("DateParserInvalidInput", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &apos;{0}&apos; FileSystem not found!.
+        ///   Looks up a localized string similar to The given type {0} is not valid!.
         /// </summary>
-        internal static string FileSystemRemotePathParserFileSystemNotFoumd {
+        internal static string NamespaceParserInvalidType {
             get {
-                return ResourceManager.GetString("FileSystemRemotePathParserFileSystemNotFoumd", resourceCulture);
+                return ResourceManager.GetString("NamespaceParserInvalidType", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The value from ({0}) could not be extracted!.
+        ///   Looks up a localized string similar to Input value can&apos;t be empty or null..
         /// </summary>
-        internal static string FileSystemSizeParserCannotExtractValue {
+        internal static string NewtonsoftDeserializerValueCanNotBeEmpty {
             get {
-                return ResourceManager.GetString("FileSystemSizeParserCannotExtractValue", resourceCulture);
+                return ResourceManager.GetString("NewtonsoftDeserializerValueCanNotBeEmpty", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The given size is not valid!.
+        ///   Looks up a localized string similar to Input value can&apos;t be empty or null..
         /// </summary>
-        internal static string FileSystemSizeParserInvalidInput {
+        internal static string NewtonsoftSerializerValueCanNotBeEmpty {
             get {
-                return ResourceManager.GetString("FileSystemSizeParserInvalidInput", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Invalid Property. Order By Format: Property, Property2 ASC, Property2 DESC.
-        /// </summary>
-        internal static string FileSystemSortParserInvalidProperty {
-            get {
-                return ResourceManager.GetString("FileSystemSortParserInvalidProperty", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Invalid Property. Given sorting property name &apos;{0}&apos; is not valid..
-        /// </summary>
-        internal static string FileSystemSortParserInvalidPropertyName {
-            get {
-                return ResourceManager.GetString("FileSystemSortParserInvalidPropertyName", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Sort direction &apos;{0}&apos; for &apos;{1}&apos; is not valid..
-        /// </summary>
-        internal static string FileSystemSortParserInvalidSortDirection {
-            get {
-                return ResourceManager.GetString("FileSystemSortParserInvalidSortDirection", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Invalid Sorting string &apos;{0}&apos;. Order By Format: Property, Property2 ASC, Property2 DESC.
-        /// </summary>
-        internal static string FileSystemSortParserInvalidSortingTerm {
-            get {
-                return ResourceManager.GetString("FileSystemSortParserInvalidSortingTerm", resourceCulture);
+                return ResourceManager.GetString("NewtonsoftSerializerValueCanNotBeEmpty", resourceCulture);
             }
         }
         
@@ -147,6 +120,87 @@ namespace FlowSync.Infrastructure {
         internal static string PlugininItializingInstanceErrorMessage {
             get {
                 return ResourceManager.GetString("PlugininItializingInstanceErrorMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Plugin {0} could not found!.
+        /// </summary>
+        internal static string PluginsManagerCouldNotFoumd {
+            get {
+                return ResourceManager.GetString("PluginsManagerCouldNotFoumd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The value from ({0}) could not be extracted!.
+        /// </summary>
+        internal static string SizeParserCannotExtractValue {
+            get {
+                return ResourceManager.GetString("SizeParserCannotExtractValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The given size is not valid!.
+        /// </summary>
+        internal static string SizeParserInvalidInput {
+            get {
+                return ResourceManager.GetString("SizeParserInvalidInput", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Invalid Property. Order By Format: Property, Property2 ASC, Property2 DESC.
+        /// </summary>
+        internal static string SortParserInvalidProperty {
+            get {
+                return ResourceManager.GetString("SortParserInvalidProperty", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Invalid Property. Given sorting property name &apos;{0}&apos; is not valid..
+        /// </summary>
+        internal static string SortParserInvalidPropertyName {
+            get {
+                return ResourceManager.GetString("SortParserInvalidPropertyName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sort direction &apos;{0}&apos; for &apos;{1}&apos; is not valid..
+        /// </summary>
+        internal static string SortParserInvalidSortDirection {
+            get {
+                return ResourceManager.GetString("SortParserInvalidSortDirection", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Invalid Sorting string &apos;{0}&apos;. Order By Format: Property, Property2 ASC, Property2 DESC.
+        /// </summary>
+        internal static string SortParserInvalidSortingTerm {
+            get {
+                return ResourceManager.GetString("SortParserInvalidSortingTerm", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The selected plugin type &apos;{0}&apos; is not valid storage plugin type..
+        /// </summary>
+        internal static string StorageNormsParserInvalidStorageType {
+            get {
+                return ResourceManager.GetString("StorageNormsParserInvalidStorageType", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &apos;{0}&apos; storage not found!.
+        /// </summary>
+        internal static string StorageNormsParserStorageNotFoumd {
+            get {
+                return ResourceManager.GetString("StorageNormsParserStorageNotFoumd", resourceCulture);
             }
         }
     }
