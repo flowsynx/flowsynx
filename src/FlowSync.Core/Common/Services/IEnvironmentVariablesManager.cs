102be64c17b74f9ccc6e19aa01ceccacda02f0e3
ziagham <amin.ziagham@gmail.com>
2023-12-15 12:10:22 +0100

    Add environment varables manager interface and implementation

---
+++ src/FlowSync.Core/Common/Services/IEnvironmentVariablesManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace FlowSync.Core.Common.Services;
+
+public interface IEnvironmentVariablesManager
+{
+    string? Get(string variableName);
+    void Set(string variableName, string value);
+}
---
+++ src/FlowSync/Services/EnvironmentVariablesManager.cs
@@ -0,0 +1,59 @@
+﻿using FlowSync.Core.Common.Services;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FlowSync.Services;
+
+public class EnvironmentVariablesManager : IEnvironmentVariablesManager
+{
+    public string? Get(string variableName)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            return System.Environment.GetEnvironmentVariable(variableName, EnvironmentVariableTarget.Machine);
+
+        string? result = null;
+        var proc = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"-c echo {variableName}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+        };
+
+        proc.Start();
+        while (!proc.StandardOutput.EndOfStream)
+            result = proc.StandardOutput.ReadLine();
+
+        proc.WaitForExit();
+        return result;
+
+    }
+
+    public void Set(string variableName, string value)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            System.Environment.SetEnvironmentVariable(variableName, value, EnvironmentVariableTarget.Machine);
+            return;
+        }
+
+        var proc = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"-c export {variableName}={value}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+        };
+        proc.Start();
+        proc.WaitForExit();
+    }
+}
