eaa4b2f24182a34b0bf32eb4b4209ff4e00edc81
ziagham <amin.ziagham@gmail.com>
2024-01-02 21:30:06 +0100

    Refactoring the code in order to read and use product version in single-self-contained build

--- src/FlowSynx/Services/FlowSynxVersion.cs
+++ src/FlowSynx/Services/FlowSynxVersion.cs
@@ -1,7 +1,5 @@
 ﻿using System.Diagnostics;
-using System.Reflection;
 using EnsureThat;
-using FlowSynx.Core.Exceptions;
 using FlowSynx.Environment;
 
 namespace FlowSynx.Services;
@@ -9,7 +7,6 @@ namespace FlowSynx.Services;
 public class FlowSynxVersion : IVersion
 {
     private readonly ILogger<FlowSynxLocation> _logger;
-    private readonly string? _rootLocation = Path.GetDirectoryName(System.AppContext.BaseDirectory);
 
     public FlowSynxVersion(ILogger<FlowSynxLocation> logger)
     {
@@ -19,28 +16,15 @@ public class FlowSynxVersion : IVersion
 
     public string Version => GetApplicationVersion();
 
-    #region MyRegion
+    #region Private function
     private string GetApplicationVersion()
     {
-        Assembly? thisAssembly = null;
-        try
-        {
-            thisAssembly = Assembly.GetEntryAssembly();
-        }
-        finally
-        {
-            if (thisAssembly is null)
-            {
-                _logger.LogWarning(Resources.FlowSynxVersionEntryAssemblyNotFound);
-                thisAssembly = Assembly.GetExecutingAssembly();
-            }
-        }
+        var assemblyLocation = System.Environment.ProcessPath;
 
-        if (thisAssembly == null)
-            throw new ApiBaseException(Resources.FlowSynxVersionErrorInReadingExecutableApplication);
+        if (string.IsNullOrEmpty(assemblyLocation))
+            return "1.0.0.0";
 
-        var fullAssemblyName = thisAssembly.Location;
-        var versionInfo = FileVersionInfo.GetVersionInfo(fullAssemblyName);
+        var versionInfo = FileVersionInfo.GetVersionInfo(assemblyLocation);
         return versionInfo.ProductVersion ?? "1.0.0.0";
     }
     #endregion
