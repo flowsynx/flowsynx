ea9736e9e069ca8d44ec4b2018c1e09522554817
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-02-26 19:01:37 +0100

    Reffactoring the code in order to read correct product version in linux version

--- src/FlowSynx/Services/FlowSynxVersion.cs
+++ src/FlowSynx/Services/FlowSynxVersion.cs
@@ -1,31 +1,17 @@
-﻿using System.Diagnostics;
-using EnsureThat;
+﻿using System.Reflection;
 using FlowSynx.Environment;
 
 namespace FlowSynx.Services;
 
 public class FlowSynxVersion : IVersion
 {
-    private readonly ILogger<FlowSynxLocation> _logger;
-
-    public FlowSynxVersion(ILogger<FlowSynxLocation> logger)
-    {
-        EnsureArg.IsNotNull(logger, nameof(logger));
-        _logger = logger;
-    }
-
     public string Version => GetApplicationVersion();
 
     #region Private function
     private string GetApplicationVersion()
     {
-        var assemblyLocation = System.Environment.ProcessPath;
-
-        if (string.IsNullOrEmpty(assemblyLocation))
-            return "1.0.0.0";
-
-        var versionInfo = FileVersionInfo.GetVersionInfo(assemblyLocation);
-        return versionInfo.ProductVersion ?? "1.0.0.0";
+        var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false);
+        return attributes.Length == 0 ? "" : ((AssemblyInformationalVersionAttribute)attributes[0]).InformationalVersion;
     }
     #endregion
 }
