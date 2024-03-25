50ff7c639ffbed298a69795fe36b5c059d7881f4
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-03-25 21:53:01 +0100

    add specification class in order to map the json string to the model.

---
+++ plugins/FlowSynx.Plugin.Storage.Azure.Files/AzureFilesSpecifications.cs
@@ -0,0 +1,9 @@
+﻿namespace FlowSynx.Plugin.Storage.Azure.Files;
+
+internal class AzureFilesSpecifications
+{
+    public string? ShareName { get; set; }
+    public string? AccountKey { get; set; }
+    public string? AccountName { get; set; }
+    public string? ConnectionString { get; set; }
+}
