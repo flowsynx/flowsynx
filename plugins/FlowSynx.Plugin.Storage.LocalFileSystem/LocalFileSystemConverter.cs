ca5c0047ba37b9e11c72530c5e78a668e57e5fc4
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-03-31 22:49:14 +0200

    add metadata to the localfilesystem

--- plugins/FlowSynx.Plugin.Storage.LocalFileSystem/LocalFileSystemConverter.cs
+++ plugins/FlowSynx.Plugin.Storage.LocalFileSystem/LocalFileSystemConverter.cs
@@ -1,4 +1,5 @@
 ﻿using FlowSynx.Security;
+using System.IO;
 
 namespace FlowSynx.Plugin.Storage.LocalFileSystem;
 
@@ -6,22 +7,28 @@ static class LocalFileSystemConverter
 {
     public static StorageEntity ToEntity(DirectoryInfo directory)
     {
-        return new StorageEntity(directory.FullName.ToUnixPath(), StorageEntityItemKind.Directory)
+        var entity = new StorageEntity(directory.FullName.ToUnixPath(), StorageEntityItemKind.Directory)
         {
             CreatedTime = directory.CreationTime,
             ModifiedTime = directory.LastWriteTime,
             Size = null
         };
+
+        entity.TryAddMetadata("Attributes", directory.Attributes.ToString());
+        return entity;
     }
 
     public static StorageEntity ToEntity(FileInfo file, bool? hashing)
     {
-        return new StorageEntity(file.FullName.ToUnixPath(), StorageEntityItemKind.File)
+        var entity = new StorageEntity(file.FullName.ToUnixPath(), StorageEntityItemKind.File)
         {
             CreatedTime = file.CreationTimeUtc,
             ModifiedTime = file.LastWriteTimeUtc,
             Size = file.Length,
-            Md5 = hashing is true ? HashHelper.GetMd5HashFile(file.FullName) : null,
+            Md5 = hashing is true ? HashHelper.GetMd5HashFile(file.FullName) : null
         };
+        
+        entity.TryAddMetadata("Attributes", file.Attributes.ToString());
+        return entity;
     }
 }
