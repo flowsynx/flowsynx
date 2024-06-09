e216257e3cc2025f331fe76f099e544db783b0db
ziagham <amin.ziagham@outlook.com>
2024-06-09 11:34:44 +0200

    delete WithGroupName flount api to make OpenApi to generate api's specifications

--- src/FlowSynx/Extensions/WebApplicationExtensions.cs
+++ src/FlowSynx/Extensions/WebApplicationExtensions.cs
@@ -11,7 +11,6 @@ public static class WebApplicationExtensions
 
         return app
             .MapGroup($"/{groupName}")
-            .WithGroupName(groupName)
             .WithTags(groupName);
     }
 
