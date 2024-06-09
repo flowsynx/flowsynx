428b822c6a0eaae53d0c503298dc6592db9812be
ziagham <amin.ziagham@outlook.com>
2024-06-09 11:35:27 +0200

    add WithOpenApi flount api to make application support OpenApi functionality

--- src/FlowSynx/Extensions/EndpointRouteBuilderExtensions.cs
+++ src/FlowSynx/Extensions/EndpointRouteBuilderExtensions.cs
@@ -4,25 +4,37 @@ public static class EndpointRouteBuilderExtensions
 {
     public static IEndpointRouteBuilder MapGet(this IEndpointRouteBuilder builder, Delegate handler, string pattern = "")
     {
-        builder.MapGet(pattern, handler).WithName(handler.Method.Name);
+        builder.MapGet(pattern, handler)
+            .WithName(handler.Method.Name)
+            .WithOpenApi();
+
         return builder;
     }
 
     public static IEndpointRouteBuilder MapPost(this IEndpointRouteBuilder builder, Delegate handler, string pattern = "")
     {
-        builder.MapPost(pattern, handler).WithName(handler.Method.Name);
+        builder.MapPost(pattern, handler)
+            .WithName(handler.Method.Name)
+            .WithOpenApi();
+
         return builder;
     }
 
     public static IEndpointRouteBuilder MapPut(this IEndpointRouteBuilder builder, Delegate handler, string pattern)
     {
-        builder.MapPut(pattern, handler).WithName(handler.Method.Name);
+        builder.MapPut(pattern, handler)
+            .WithName(handler.Method.Name)
+            .WithOpenApi();
+
         return builder;
     }
 
     public static IEndpointRouteBuilder MapDelete(this IEndpointRouteBuilder builder, Delegate handler, string pattern)
     {
-        builder.MapDelete(pattern, handler).WithName(handler.Method.Name);
+        builder.MapDelete(pattern, handler)
+            .WithName(handler.Method.Name)
+            .WithOpenApi();
+
         return builder;
     }
 }
