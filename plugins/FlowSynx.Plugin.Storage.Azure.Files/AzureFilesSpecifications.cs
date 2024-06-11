05bcc7cc2fe98a79b816ef4192fcaa19953df59d
ziagham <amin.ziagham@outlook.com>
2024-06-11 09:53:42 +0200

    swaping the property places based on the priority. AccountName fiest and then AccountKey. #6

--- plugins/FlowSynx.Plugin.Storage.Azure.Files/AzureFilesSpecifications.cs
+++ plugins/FlowSynx.Plugin.Storage.Azure.Files/AzureFilesSpecifications.cs
@@ -7,9 +7,9 @@ public class AzureFilesSpecifications
     [RequiredMember]
     public string? ShareName { get; set; }
 
-    public string? AccountKey { get; set; }
-
     public string? AccountName { get; set; }
+
+    public string? AccountKey { get; set; }
 
     public string? ConnectionString { get; set; }
 }
