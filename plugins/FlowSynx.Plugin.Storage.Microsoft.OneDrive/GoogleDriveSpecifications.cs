b441ad646915bbe717c061de159d38ba7427414d
ziagham <amin.ziagham@outlook.com>
2024-07-17 00:16:20 +0200

    Add Google Drive plugin project and add related packages as well as adding Specification class #53

---
+++ plugins/FlowSynx.Plugin.Storage.Microsoft.OneDrive/GoogleDriveSpecifications.cs
@@ -0,0 +1,39 @@
+﻿using FlowSynx.Plugin.Abstractions;
+
+namespace FlowSynx.Plugin.Storage.Google.Drive;
+
+internal class GoogleDriveSpecifications
+{
+    [RequiredMember]
+    public string? ProjectId { get; set; }
+
+    [RequiredMember]
+    public string? PrivateKeyId { get; set; }
+
+    [RequiredMember]
+    public string? PrivateKey { get; set; }
+
+    [RequiredMember]
+    public string? ClientEmail { get; set; }
+
+    [RequiredMember]
+    public string? ClientId { get; set; }
+
+    public string? Scope { get; set; } = "drive.readonly";
+
+    public string ApplicationScope => string.IsNullOrEmpty(Scope) ? 
+                                      string.Empty : 
+                                      $"https://www.googleapis.com/auth/{Scope}";
+
+    public string Type => "service_account";
+
+    public string AuthUri => "https://accounts.google.com/o/oauth2/auth";
+
+    public string TokenUri => "https://oauth2.googleapis.com/token";
+
+    public string AuthProviderX509CertUrl => "https://www.googleapis.com/oauth2/v1/certs";
+
+    public string ClientX509CertUrl => $"https://www.googleapis.com/robot/v1/metadata/x509/{ClientEmail}";
+
+    public string UniverseDomain => "googleapis.com";
+}
