de0fd91ebfc3aeceeb53b40d075e1e6da99e3b7e
ziagham <amin.ziagham@outlook.com>
2024-07-21 11:41:58 +0200

    Change nullable string value to string in Specifications class of Azure Files plugin #69

--- plugins/FlowSynx.Plugin.Storage.Azure.Files/AzureFilesSpecifications.cs
+++ plugins/FlowSynx.Plugin.Storage.Azure.Files/AzureFilesSpecifications.cs
@@ -5,11 +5,11 @@ namespace FlowSynx.Plugin.Storage.Azure.Files;
 public class AzureFilesSpecifications
 {
     [RequiredMember]
-    public string? ShareName { get; set; }
+    public string ShareName { get; set; } = string.Empty;
 
-    public string? AccountName { get; set; }
+    public string AccountName { get; set; } = string.Empty;
 
-    public string? AccountKey { get; set; }
+    public string AccountKey { get; set; } = string.Empty;
 
-    public string? ConnectionString { get; set; }
+    public string ConnectionString { get; set; } = string.Empty;
 }
