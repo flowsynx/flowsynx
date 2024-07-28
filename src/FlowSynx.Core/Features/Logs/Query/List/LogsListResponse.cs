d917c45af749a904097c90c8bbdde542eaec37b5
ziagham <amin.ziagham@outlook.com>
2024-07-28 14:27:48 +0200

    Correct ordering of properties for LogsListResponse #73

--- src/FlowSynx.Core/Features/Logs/Query/List/LogsListResponse.cs
+++ src/FlowSynx.Core/Features/Logs/Query/List/LogsListResponse.cs
@@ -2,9 +2,9 @@
 
 public class LogsListResponse
 {
-    public string? UserName { get; set; }
-    public string? Machine { get; set; }
     public DateTime TimeStamp { get; set; }
-    public required string Message { get; set; }
     public required string Level { get; set; }
+    public string? Machine { get; set; }
+    public string? UserName { get; set; }
+    public required string Message { get; set; }
 }
