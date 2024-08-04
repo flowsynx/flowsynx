2232b433211ee0c5079283099e76b5ec2655fa35
ziagham <amin.ziagham@outlook.com>
2024-08-04 18:21:08 +0200

    Add new Plugin for managing Dropbox storage #85

--- plugins/FlowSynx.Plugin.Storage.Dropbox/DropboxStorageSpecifications.cs
+++ plugins/FlowSynx.Plugin.Storage.Dropbox/DropboxStorageSpecifications.cs
@@ -1,6 +1,9 @@
-﻿namespace FlowSynx.Plugin.Storage.Dropbox;
+﻿using FlowSynx.Plugin.Abstractions;
+
+namespace FlowSynx.Plugin.Storage.Dropbox;
 
 internal class DropboxStorageSpecifications
 {
-
+    [RequiredMember]
+    public string AccessToken { get; set; } = string.Empty;
 }
