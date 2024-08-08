8a36dd75a9114ed8520ba81b5668325d3a78de13
ziagham <amin.ziagham@outlook.com>
2024-08-08 12:24:07 +0200

    Delete Dropbox plugin #88

--- plugins/FlowSynx.Plugin.Storage.Dropbox/DropboxStorageSpecifications.cs
+++
@@ -1,9 +0,0 @@
-﻿using FlowSynx.Plugin.Abstractions;
-
-namespace FlowSynx.Plugin.Storage.Dropbox;
-
-internal class DropboxStorageSpecifications
-{
-    [RequiredMember]
-    public string AccessToken { get; set; } = string.Empty;
-}
