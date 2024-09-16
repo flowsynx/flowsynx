ea8f8f41b79bc7935079622d7e4e4754be24b364
ziagham <amin.ziagham@outlook.com>
2024-09-17 00:17:13 +0200

    Delete Check feature #154

--- src/FlowSynx.Core/Features/Check/Command/CheckResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlowSynx.Core.Features.Check.Command;
-
-public class CheckResponse
-{
-    public string? Id { get; set; }
-    public string? Name { get; set; }
-    public string? Path { get; set; }
-    public string State { get; set; } = string.Empty;
-}
