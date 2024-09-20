a211d79899a50ec218811284927a3d3d0c950f60
ziagham <amin.ziagham@outlook.com>
2024-09-20 15:14:03 +0200

    Add a class for handling and managing Csv data #142

---
+++ plugins/FlowSynx.Plugin.Stream.Csv/CsvLoader.cs
@@ -0,0 +1,125 @@
+﻿using System.Data;
+using System.Text;
+using FlowSynx.IO.Serialization;
+using Microsoft.Extensions.Logging;
+
+namespace FlowSynx.Plugin.Stream.Csv;
+
+internal class CsvLoader
+{
+    private readonly ILogger _logger;
+    private readonly ISerializer _serializer;
+
+    public CsvLoader(ILogger logger, ISerializer serializer)
+    {
+        _logger = logger;
+        _serializer = serializer;
+    }
+
+    public DataTable Load(string fullPath, string delimiter, bool? includeMetaData)
+    {
+        var dataTable = new DataTable();
+        using var streamReader = new StreamReader(fullPath);
+        var firstLine = streamReader.ReadLine();
+        var headers = firstLine?.Split(delimiter, StringSplitOptions.None);
+
+        if (includeMetaData is true)
+            headers = headers?.Append("Metadata").ToArray();
+        
+        if (headers != null)
+        {
+            foreach (var header in headers)
+                dataTable.Columns.Add(header);
+
+            var columnInterval = headers.Count();
+            var newLine = streamReader.ReadLine();
+            while (newLine != null)
+            {
+                object?[] fields = newLine.Split(delimiter, StringSplitOptions.None);
+                if (includeMetaData is true)
+                {
+                    var metadataObject = GetMetaData(fields);
+                    fields = fields.Append(metadataObject).ToArray();
+                }
+
+                var currentLength = fields.Count();
+                if (currentLength < columnInterval)
+                {
+                    while (currentLength < columnInterval)
+                    {
+                        newLine += streamReader.ReadLine();
+                        currentLength = newLine.Split(delimiter, StringSplitOptions.None).Count();
+                    }
+
+                    fields = newLine.Split(delimiter, StringSplitOptions.None);
+                }
+
+                if (currentLength > columnInterval)
+                {
+                    newLine = streamReader.ReadLine();
+                    continue;
+                }
+
+                if (!fields.Any())
+                    continue;
+
+                dataTable.Rows.Add(fields);
+                newLine = streamReader.ReadLine();
+            }
+        }
+
+        streamReader.Close();
+        return dataTable;
+    }
+
+    public string ToCsv(DataTable dataTable, string delimiter)
+    {
+        var stringBuilder = new StringBuilder();
+
+        var columnNames = dataTable.Columns.Cast<DataColumn>().Select(column => column.ColumnName);
+        stringBuilder.AppendLine(string.Join(delimiter, columnNames));
+
+        foreach (DataRow row in dataTable.Rows)
+        {
+            var fields = row.ItemArray.Select(field => field is null ? string.Empty : field.ToString());
+            stringBuilder.AppendLine(string.Join(delimiter, fields));
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    public void Delete(DataTable allRows, DataTable rowsToDelete)
+    {
+        foreach (DataRow rowToDelete in rowsToDelete.Rows)
+        {
+            foreach (DataRow row in allRows.Rows)
+            {
+                var rowToDeleteArray = rowToDelete.ItemArray;
+                var rowArray = row.ItemArray;
+                var equalRows = true;
+                for (var i = 0; i < rowArray.Length; i++)
+                {
+                    if (!rowArray[i]!.Equals(rowToDeleteArray[i]))
+                    {
+                        equalRows = false;
+                    }
+                }
+
+                if (!equalRows) 
+                    continue;
+
+                allRows.Rows.Remove(row);
+                break;
+            }
+        }
+    }
+
+    private object GetMetaData(object content)
+    {
+        var contentHash = FlowSynx.Security.HashHelper.Md5.GetHash(content);
+        return new
+        {
+            ContentHash = contentHash
+        };
+    }
+}
