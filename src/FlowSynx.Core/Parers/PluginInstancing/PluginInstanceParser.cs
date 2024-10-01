cfa41c387c18860805dfcb2f8e533e847f88f9fc
ziagham <amin.ziagham@outlook.com>
2024-10-02 00:01:47 +0200

    Refactoring PluginInstanceParser to support multiple plugins types for stream plugins #183

--- src/FlowSynx.Core/Parers/PluginInstancing/PluginInstanceParser.cs
+++ src/FlowSynx.Core/Parers/PluginInstancing/PluginInstanceParser.cs
@@ -25,6 +25,7 @@ internal class PluginInstanceParser : IPluginInstanceParser
     private readonly ISerializer _serializer;
     private readonly IServiceProvider _serviceProvider;
     private const string ParserSeparator = "://";
+    private const string PipelineSeparator = "|";
 
     public PluginInstanceParser(ILogger<PluginInstanceParser> logger, IConfigurationManager configurationManager,
         IPluginsManager pluginsManager, ILogger<LocalFileSystemStorage> localStorageLogger,
@@ -53,6 +54,7 @@ internal class PluginInstanceParser : IPluginInstanceParser
     {
         try
         {
+            var entity = string.Empty;
             var segments = path.Split(ParserSeparator);
 
             if (segments.Length != 2)
@@ -60,20 +62,40 @@ internal class PluginInstanceParser : IPluginInstanceParser
                 return CreateStorageNormsInfoInstance(GetLocalFileSystemStoragePlugin(), "LocalFileSystem", null, path);
             }
 
-            var fileSystemExist = _configurationManager.IsExist(segments[0]);
-            if (!fileSystemExist)
+            entity = segments[1];
+            var pipelineSegments = segments[0].Split(PipelineSeparator);
+            if (pipelineSegments.Length > 2)
+                throw new StorageNormsParserException("Pipeline is not applied correctly.");
+
+            string primaryConfigName = string.Empty;
+            string secondaryConfigName = string.Empty;
+            if (pipelineSegments.Length == 1)
             {
-                return CreateStorageNormsInfoInstance(GetLocalFileSystemStoragePlugin(), "LocalFileSystem", null, path);
+                primaryConfigName = pipelineSegments[0];
+            }
+            else
+            {
+                primaryConfigName = pipelineSegments[0];
+                secondaryConfigName = pipelineSegments[1];
             }
 
-            var fileSystem = _configurationManager.Get(segments[0]);
+            var primaryConfigNameExist = _configurationManager.IsExist(primaryConfigName);
 
-            //if (_namespaceParser.Parse(fileSystem.Type) != PluginNamespace.Storage 
-            //    || _namespaceParser.Parse(fileSystem.Type) != PluginNamespace.Stream)
-            //    throw new StorageNormsParserException(string.Format(Resources.StorageNormsParserInvalidStorageType, fileSystem.Type));
+            if (!primaryConfigNameExist)
+                throw new StorageNormsParserException($"{primaryConfigName} is exist.");
 
-            var plugin = _pluginsManager.Get(fileSystem.Type);
-            return CreateStorageNormsInfoInstance(plugin, segments[0], fileSystem.Specifications.ToSpecifications(), segments[1]);
+            var primaryConfig = _configurationManager.Get(primaryConfigName);
+            if (_namespaceParser.Parse(primaryConfig.Type) == PluginNamespace.Stream)
+            {
+                var secondaryConfigNameExist = _configurationManager.IsExist(secondaryConfigName);
+                if (!secondaryConfigNameExist)
+                    throw new StorageNormsParserException($"{secondaryConfigName} is exist.");
+            }
+            else if (_namespaceParser.Parse(primaryConfig.Type) != PluginNamespace.Stream && !string.IsNullOrEmpty(secondaryConfigName))
+                throw new StorageNormsParserException($"{primaryConfigName} not supporting multiple entity.");
+
+            var plugin = _pluginsManager.Get(primaryConfig.Type);
+            return CreateStorageNormsInfoInstance(plugin, segments[0], primaryConfig.Specifications.ToSpecifications(), entity);
         }
         catch (Exception ex)
         {
