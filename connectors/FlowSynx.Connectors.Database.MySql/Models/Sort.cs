c0c84135e631bf901b38db75b1d50a7c13b1b3af
ziagham <amin.ziagham@outlook.com>
2024-11-07 16:06:56 +0100

    Add Sort class to the MySql connector project #232

---
+++ connectors/FlowSynx.Connectors.Database.MySql/Models/Sort.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowSynx.Connectors.Database.MySql.Models;
+
+public class Sort
+{
+    public required string Name { get; set; }
+    public string? Direction { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {Direction}";
+    }
+}
