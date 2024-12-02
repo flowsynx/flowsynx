e186fa77f43afc284af7cf0061ee6f5d16248462
ziagham <amin.ziagham@outlook.com>
2024-12-02 22:20:16 +0100

    Implement ProcessTransferAsync method #223

--- connectors/FlowSynx.Connectors.Database.MySql/Services/MysqlDatabaseManager.cs
+++ connectors/FlowSynx.Connectors.Database.MySql/Services/MysqlDatabaseManager.cs
@@ -12,6 +12,8 @@ using FlowSynx.Data.Sql;
 using FlowSynx.Data.Extensions;
 using FlowSynx.Data.Sql.Builder;
 using System.Text;
+using System.Threading;
+using System.Runtime.ConstrainedExecution;
 
 namespace FlowSynx.Connectors.Database.MySql.Services;
 
@@ -52,11 +54,7 @@ public class MysqlDatabaseManager: IMysqlDatabaseManager
         var createOptions = context.Options.ToObject<CreateOptions>();
 
         var createTableOption = GetCreateOption(createOptions);
-        var sql = sqlOptions.Sql ?? _sqlBuilder.Create(_format, createTableOption);
-        
-        var command = new MySqlCommand(sql, _connection);
-        var rowsAffected = await command.ExecuteNonQueryAsync(cancellationToken);
-        _logger.LogInformation($"Created {rowsAffected} row(s)!");
+        await CreateTableAsync(createTableOption, cancellationToken);
     }
 
     public async Task WriteAsync(Context context, CancellationToken cancellationToken)
@@ -130,8 +128,8 @@ public class MysqlDatabaseManager: IMysqlDatabaseManager
         var sqlOptions = context.Options.ToObject<SqlOptions>();
         var existOptions = context.Options.ToObject<ExistOptions>();
 
-        var existtSqlOption = GetExistOption(existOptions);
-        var sql = sqlOptions.Sql ?? _sqlBuilder.Exist(_format, existtSqlOption);
+        var existtSqlOption = GetExistRecordOption(existOptions);
+        var sql = sqlOptions.Sql ?? _sqlBuilder.ExistRecord(_format, existtSqlOption);
         
         var command = new MySqlCommand(sql, _connection);
         var reader = await command.ExecuteReaderAsync(cancellationToken);
@@ -172,15 +170,60 @@ public class MysqlDatabaseManager: IMysqlDatabaseManager
         var createOptions = context.Options.ToObject<CreateOptions>();
         var writeOptions = context.Options.ToObject<WriteOptions>();
 
-        await BulkInsert(transferData, "", cancellationToken).ConfigureAwait(false);
+        //var tableExistSql = _sqlBuilder.ExistTable(_format, new ExistTableOption { Table = writeOptions.Table });
+        //var command = new MySqlCommand(tableExistSql, _connection);
+        //var reader = await command.ExecuteReaderAsync(cancellationToken);
+        //if (reader != null && reader.HasRows)
+        //{
+        //    var tableColumns = await GetColumnsNames(writeOptions.Table, cancellationToken);
+        //    var transferColumns = transferData.Columns.Select(c => c.Name).AsEnumerable();
+        //    bool allExist = transferColumns.All(item => tableColumns.Contains(item));
+
+        //    if (!allExist)
+        //        throw new DatabaseException("Table fields are not matched!");
+        //}
+        //else
+        //{
+            var tableFieldList = new CreateTableFieldList();
+            tableFieldList.AddRange(transferData.Columns.Select(x => new CreateTableField { Name = x.Name, Type = _format.GetDbType(x.DataType) }));
+
+            var tableCreateOption = new CreateOption
+            {
+                Table = writeOptions.Table,
+                Fields = tableFieldList
+            };
+
+        await CreateTableAsync(tableCreateOption, cancellationToken);
+        //}
+
+        await BulkInsert(writeOptions.Table, transferData, cancellationToken).ConfigureAwait(false);
     }
 
-    private async Task BulkInsert(TransferData transferData, string tableName, CancellationToken cancellationToken)
+    private async Task<IEnumerable<string>> GetColumnsNames(string tableName, CancellationToken cancellationToken)
+    {
+        List<string> listacolumnas = new List<string>();
+        var tableExistSql = _sqlBuilder.TableFields(_format, new TableFieldsOption { Table = tableName });
+        var command = new MySqlCommand(tableExistSql, _connection);
+        using (var reader = await command.ExecuteReaderAsync(cancellationToken))
+        {
+            reader.Read();
+
+            var table = reader.GetSchemaTable();
+            foreach (DataColumn column in table.Columns)
+            {
+                listacolumnas.Add(column.ColumnName);
+            }
+        }
+
+        return listacolumnas;
+    }
+
+    private async Task BulkInsert(string tableName, TransferData transferData, CancellationToken cancellationToken)
     {
         try
         {
             StringBuilder sb = new StringBuilder();
-            var columnNames = transferData.Columns.ToArray();
+            var columnNames = transferData.Columns.Select(x=>x.Name).ToArray();
             var columns = string.Join(",", columnNames);
             sb.Append($"INSERT INTO {tableName} ({columns}) VALUES ");
 
@@ -188,9 +231,9 @@ public class MysqlDatabaseManager: IMysqlDatabaseManager
             {
                 foreach (var dr in transferData.Rows)
                 {
-                    if (dr != null)
+                    if (dr != null && dr.Items != null)
                     {
-                        var row = string.Join(",", dr.Items.Select(x=>x.ToString()));
+                        var row = string.Join(",", dr.Items.Select(GetValue));
                         sb.Append($"({row}),");
                     }
                 }
@@ -260,7 +303,7 @@ public class MysqlDatabaseManager: IMysqlDatabaseManager
         Paging = GetPaging(options.Paging)
     };
 
-    private ExistOption GetExistOption(ExistOptions options) => new()
+    private ExistRecordOption GetExistRecordOption(ExistOptions options) => new()
     {
         Table = options.Table,
         Filter = GetFilterList(options.Filter)
@@ -370,6 +413,22 @@ public class MysqlDatabaseManager: IMysqlDatabaseManager
         }
 
         return result;
+    }
+
+    private async Task CreateTableAsync(CreateOption createOption, CancellationToken cancellationToken)
+    {
+        var sql = _sqlBuilder.Create(_format, createOption);
+        var command = new MySqlCommand(sql, _connection);
+        var rowsAffected = await command.ExecuteNonQueryAsync(cancellationToken);
+        _logger.LogInformation($"Created {rowsAffected} row(s)!");
+    }
+
+    private object? GetValue(object? obj)
+    {
+        if (obj?.GetType() == typeof(string))
+            return $"'{obj.ToString()}'";
+
+        return obj;
     }
     #endregion
 }
