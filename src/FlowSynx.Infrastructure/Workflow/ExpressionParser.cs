7e20ba64bb2a4410b1334ed9ebac29d82caab4a8
ziagham <amin.ziagham@outlook.com>
2025-03-18 23:29:13 +0100

    Add ExpressionParser to parse outputs from task steps as well as delete unused related classess #274

---
+++ src/FlowSynx.Infrastructure/Workflow/ExpressionParser.cs
@@ -0,0 +1,38 @@
+﻿namespace FlowSynx.Infrastructure.Workflow;
+
+public class ExpressionParser
+{
+    private Dictionary<string, object?> _outputs;
+
+    public ExpressionParser(Dictionary<string, object?> outputs)
+    {
+        _outputs = outputs;
+    }
+
+    public object? Parse(string? expression)
+    {
+        if (expression == null)
+            return expression;
+
+        expression = expression.Trim();
+
+        const string prefix = "$[outputs('";
+        const string suffix = "')]";
+
+        if (expression.StartsWith("$[outputs('") && expression.EndsWith("')]"))
+        {
+            string outputKey = expression.Substring(prefix.Length, expression.Length - prefix.Length - suffix.Length);
+
+            if (_outputs.ContainsKey(outputKey))
+            {
+                return _outputs[outputKey];
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Output '{outputKey}' not found.");
+            }
+        }
+
+        return expression;
+    }
+}
