7be4c092b3e6875c2416cd1059e1d1cd3aad5676
ziagham <amin.ziagham@outlook.com>
2025-03-24 22:37:43 +0100

    Clean up code #284

--- src/FlowSynx.Application/Parers/Specifications/SpecificationsResult.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace FlowSynx.Application.Parers.Specifications;
-
-//public class PluginSpecificationsResult
-//{
-//    public bool Valid { get; set; }
-//    public string? Message { get; set; }
-//}
--- src/FlowSynx.Infrastructure/Workflow/WorkflowFunction.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace FlowSynx.Infrastructure.Workflow;
-
-//public abstract class WorkflowFunction
-//{
-//    public abstract void ValidateArguments(List<object> arguments);
-//    public abstract object Transform(object? value, List<object> arguments);
-//}
