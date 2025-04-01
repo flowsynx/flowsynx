73dd77cb057462eb359db5d1ef2bedb0db4f2d84
ziagham <amin.ziagham@outlook.com>
2025-04-01 21:42:10 +0200

    Reorganizing Error codes and correcting appsetting user id #299

--- src/FlowSynx.Application/Models/ErrorCode.cs
+++ src/FlowSynx.Application/Models/ErrorCode.cs
@@ -2,117 +2,117 @@
 
 public enum ErrorCode
 {
-    None = 0,
+    None                                            = 0,
 
     #region Application error codes
-    ApplicationEndpoint = 1,
-    ApplicationLocation = 2,
-    ApplicationVersion = 3,
-    ApplicationTriggerProcessing = 4,
-    ApplicationHealthCheck = 5,
-    ApplicationConfigureServer = 6,
-    ApplicationOpenApiService = 7,
-    #endregion
-
-    #region Logging error codes
-    LogsList = 1001,
-    LogGetItem = 1002,
-    LogAdd = 1003,
-    LoggerCreation = 1004,
-    #endregion
-
-    #region Auding error codes
-    AuditsApplying = 1101,
-    AuditsGetList = 1102,
-    AuditGetItem = 1103,
-    #endregion
-
-    #region Plugin Configuration error codes
-    PluginConfiguration = 1201,
-    PluginConfigurationNotFound = 1202,
-    PluginConfigurationIsAlreadyExist = 1203,
-    PluginConfigurationList = 1204,
-    PluginConfigurationGetItem = 1205,
-    PluginConfigurationCheckExistence = 1206,
-    PluginConfigurationAdd = 1207,
-    PluginConfigurationUpdate = 1208,
-    PluginConfigurationDelete = 1209,
-    #endregion
-
-    #region Plugins error codes
-    PluginNotFound = 1301,
-    PluginTypeNotFound = 1302,
-    PluginSpecificationsInvalid = 1303,
-    PluginTypeGetItem = 1304,
-    #endregion
-
-    #region Serialization error codes
-    Serialization = 1402,
-    #endregion
-
-    #region Validation error codes
-    InputValidation = 1501,
-    #endregion
-
-    #region Database error codes
-    DatabaseConnection = 1601,
-    DatabaseCreation = 1602,
-    DatabaseDataSeeder = 1603,
-    DatabaseSaveData = 1604,
-    DatabaseDeleteData = 1605,
-    DatabaseModelCreating = 1606,
-    DatabaseTransaction = 1607,
-    #endregion
-
-    #region Security error codes
-    SecurityGetUserId = 1701,
-    SecurityGetUserName = 1702,
-    SecurityCheckIsAuthenticated = 1703,
-    SecurityGetUserRoles = 1704,
-    SecurityBasicAuthenticationMustHaveUniqueNames = 1705,
-    SecurityInitializedError = 1706,
-    SecurityAthenticationIsRequired = 1750,
-    #endregion
-
-    #region Workflow error codes
-    WorkflowNotFound = 1801,
-    WorkflowMustBeNotEmpty = 1802,
-    WorkflowNameMustHaveValue = 1803,
-    WorkflowHasDuplicateNames = 1804,
-    WorkflowMissingDependencies = 1805,
-    WorkflowCyclicDependencies = 1806,
-    WorkflowsGetList = 1807,
-    WorkflowGetItem = 1808,
-    WorkflowCheckExistence = 1809,
-    WorkflowAdd = 1810,
-    WorkflowUpdate = 1811,
-    WorkflowDelete = 1812,
-    WorkflowsGetExecutionList = 1813,
-    WorkflowGetExecutionItem = 1814,
-    WorkflowExecutionCheckExistence = 1815,
-    WorkflowExecutionAdd = 1816,
-    WorkflowExecutionUpdate = 1817,
-    WorkflowExecutionDelete = 1818,
-    WorkflowTaskExecutionsList = 1819,
-    WorkflowGetTaskExecutionItem = 1820,
-    WorkflowTaskExecutionAdd = 1821,
-    WorkflowTaskExecutionUpdate = 1822,
-    WorkflowTaskExecutionDelete = 1823,
-    WorkflowTriggersList = 1824,
-    WorkflowActiveTriggersList = 1825,
-    WorkflowGetTriggerItem = 1826,
-    WorkflowTriggersAdd = 1827,
-    WorkflowTriggersUpdate = 1828,
-    WorkflowTriggersDelete = 1829,
+    ApplicationEndpoint                             = 1001,
+    ApplicationLocation                             = 1002,
+    ApplicationVersion                              = 1003,
+    ApplicationTriggerProcessing                    = 1004,
+    ApplicationHealthCheck                          = 1005,
+    ApplicationConfigureServer                      = 1006,
+    ApplicationOpenApiService                       = 1007,
     #endregion
 
     #region Behavior error codes
-    BehaviorUnhandledException = 2001,
-    BehaviorPerformanceLongRunning = 2002,
-    BehaviorPerformanceError = 2003,
+    BehaviorUnhandledException                      = 1201,
+    BehaviorPerformanceLongRunning                  = 1202,
+    BehaviorPerformanceError                        = 1203,
+    #endregion
+
+    #region Serialization error codes
+    Serialization                                   = 1302,
+    #endregion
+
+    #region Validation error codes
+    InputValidation                                 = 1401,
+    #endregion
+
+    #region Security error codes
+    SecurityGetUserId                               = 1501,
+    SecurityGetUserName                             = 1502,
+    SecurityCheckIsAuthenticated                    = 1503,
+    SecurityGetUserRoles                            = 1504,
+    SecurityBasicAuthenticationMustHaveUniqueNames  = 1505,
+    SecurityInitializedError                        = 1506,
+    SecurityAthenticationIsRequired                 = 1550,
+    #endregion
+
+    #region Database error codes
+    DatabaseConnection                              = 1601,
+    DatabaseCreation                                = 1602,
+    DatabaseDataSeeder                              = 1603,
+    DatabaseSaveData                                = 1604,
+    DatabaseDeleteData                              = 1605,
+    DatabaseModelCreating                           = 1606,
+    DatabaseTransaction                             = 1607,
+    #endregion
+
+    #region Logging error codes
+    LogsList                                        = 2001,
+    LogGetItem                                      = 2002,
+    LogAdd                                          = 2003,
+    LoggerCreation                                  = 2004,
+    #endregion
+
+    #region Auding error codes
+    AuditsApplying                                  = 2101,
+    AuditsGetList                                   = 2102,
+    AuditGetItem                                    = 2103,
+    #endregion
+
+    #region Plugin Configuration error codes
+    PluginConfiguration                             = 2201,
+    PluginConfigurationNotFound                     = 2202,
+    PluginConfigurationIsAlreadyExist               = 2203,
+    PluginConfigurationList                         = 2204,
+    PluginConfigurationGetItem                      = 2205,
+    PluginConfigurationCheckExistence               = 2206,
+    PluginConfigurationAdd                          = 2207,
+    PluginConfigurationUpdate                       = 2208,
+    PluginConfigurationDelete                       = 2209,
+    #endregion
+
+    #region Plugins error codes
+    PluginNotFound                                  = 2301,
+    PluginTypeNotFound                              = 2302,
+    PluginSpecificationsInvalid                     = 2303,
+    PluginTypeGetItem                               = 2304,
+    #endregion
+
+    #region Workflow error codes
+    WorkflowNotFound                                = 2401,
+    WorkflowMustBeNotEmpty                          = 2402,
+    WorkflowNameMustHaveValue                       = 2403,
+    WorkflowHasDuplicateNames                       = 2404,
+    WorkflowMissingDependencies                     = 2405,
+    WorkflowCyclicDependencies                      = 2406,
+    WorkflowsGetList                                = 2407,
+    WorkflowGetItem                                 = 2408,
+    WorkflowCheckExistence                          = 2409,
+    WorkflowAdd                                     = 2410,
+    WorkflowUpdate                                  = 2411,
+    WorkflowDelete                                  = 2412,
+    WorkflowsGetExecutionList                       = 2413,
+    WorkflowGetExecutionItem                        = 2414,
+    WorkflowExecutionCheckExistence                 = 2415,
+    WorkflowExecutionAdd                            = 2416,
+    WorkflowExecutionUpdate                         = 2417,
+    WorkflowExecutionDelete                         = 2418,
+    WorkflowTaskExecutionsList                      = 2419,
+    WorkflowGetTaskExecutionItem                    = 2420,
+    WorkflowTaskExecutionAdd                        = 2421,
+    WorkflowTaskExecutionUpdate                     = 2422,
+    WorkflowTaskExecutionDelete                     = 2423,
+    WorkflowTriggersList                            = 2424,
+    WorkflowActiveTriggersList                      = 2425,
+    WorkflowGetTriggerItem                          = 2426,
+    WorkflowTriggersAdd                             = 2427,
+    WorkflowTriggersUpdate                          = 2428,
+    WorkflowTriggersDelete                          = 2429,
     #endregion
 
     #region Unknown error codes
-    UnknownError = 9999
+    UnknownError                                    = 9999
     #endregion
 }
