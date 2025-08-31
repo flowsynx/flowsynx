d3430fde6dc7b0ea2f9a1570c4c7582cf2dffe97
ziagham <amin.ziagham@outlook.com>
2025-08-31 18:55:52 +0200

    Add IEventPublisher and its implementation to support event submiting #435

---
+++ src/FlowSynx.Application/Services/IEventPublisher.cs
@@ -0,0 +1,17 @@
+﻿namespace FlowSynx.Application.Services;
+
+public interface IEventPublisher
+{
+    Task PublishAsync<TEvent>(
+        string methodName,
+        TEvent @event, 
+        CancellationToken cancellationToken = default)
+        where TEvent : class;
+
+    Task PublishToUserAsync<TEvent>(
+        string userId,
+        string methodName,
+        TEvent @event, 
+        CancellationToken cancellationToken = default)
+        where TEvent : class;
+}
---
+++ src/FlowSynx/Services/SignalREventPublisher.cs
@@ -0,0 +1,41 @@
+﻿using FlowSynx.Application.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FlowSynx.Services;
+
+public class SignalREventPublisher<THub> : IEventPublisher where THub : Hub
+{
+    private readonly IHubContext<THub> _hubContext;
+
+    public SignalREventPublisher(IHubContext<THub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task PublishAsync<TEvent>(
+        string methodName,
+        TEvent @event, 
+        CancellationToken cancellationToken = default)
+        where TEvent : class
+    {
+        await _hubContext.Clients.All.SendAsync(
+            methodName,
+            @event,
+            cancellationToken
+        );
+    }
+
+    public async Task PublishToUserAsync<TEvent>(
+        string userId,
+        string methodName,
+        TEvent @event, 
+        CancellationToken cancellationToken = default)
+        where TEvent : class
+    {
+        await _hubContext.Clients.User(userId).SendAsync(
+            methodName,
+            @event,
+            cancellationToken
+        );
+    }
+}
